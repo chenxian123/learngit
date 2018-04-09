a8982beba5789ca32e88f2ec6260bb02e88a86af
123 <chenxian@localhost.localdomain>
2018-04-09 10:02:38 -0700

    2018-4

---
+++ printf.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#print 2018y4m
+
+#Monday,Tuesday,Wednesday,Thursday,Friday,Saturday,Sunday
+
+printf "%-10s %-10s %-10s %-10s %-10s %-10s %-10s\n" Monday Tuesday Wednesday thursday Friday Saturday Sunday
+printf "%-10s %-10s %-10s %-10s %-10s %-10s %-10s\n" 1 2 3 4 5 6 7
+printf "%-10s %-10s %-10s %-10s %-10s %-10s %-10s\n" 8 9 10 11 12 13 14
+printf "%-10s %-10s %-10s %-10s %-10s %-10s %-10s\n" 15 16 17 18 19 20 21
+printf "%-10s %-10s %-10s %-10s %-10s %-10s %-10s\n" 22 23 24 25 26 27 28
+printf "%-10s %-10s %-10s %-10s %-10s %-10s %-10s\n" 29 30
