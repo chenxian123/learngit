dfcffa4be27ef5572346269562d80a7885d17d36
123 <chenxian@localhost.localdomain>
2018-04-10 03:06:47 -0700

    color

--- printf.sh
+++ printf.sh
@@ -3,9 +3,13 @@
 
 #Monday,Tuesday,Wednesday,Thursday,Friday,Saturday,Sunday
 
-printf "%-10s %-10s %-10s %-10s %-10s %-10s %-10s\n" Monday Tuesday Wednesday thursday Friday Saturday Sunday
-printf "%-10s %-10s %-10s %-10s %-10s %-10s %-10s\n" 1 2 3 4 5 6 7
-printf "%-10s %-10s %-10s %-10s %-10s %-10s %-10s\n" 8 9 10 11 12 13 14
-printf "%-10s %-10s %-10s %-10s %-10s %-10s %-10s\n" 15 16 17 18 19 20 21
-printf "%-10s %-10s %-10s %-10s %-10s %-10s %-10s\n" 22 23 24 25 26 27 28
-printf "%-10s %-10s %-10s %-10s %-10s %-10s %-10s\n" 29 30
+printf "\e[1;31m%-10s %-10s %-10s %-10s %-10s %-10s %-10s \e[0m \n" Monday Tuesday Wednesday thursday Friday Saturday Sunday
+#printf "%-10s %-10s %-10s %-10s %-10s %-10s %-10s\n" 1 2 3 4 5 6 7
+#printf "%-10s %-10s %-10s %-10s %-10s %-10s %-10s\n" 8 9 10 11 12 13 14
+#printf "%-10s %-10s %-10s %-10s %-10s %-10s %-10s\n" 15 16 17 18 19 20 21
+#printf "%-10s %-10s %-10s %-10s %-10s %-10s %-10s\n" 22 23 24 25 26 27 28
+#printf "%-10s %-10s %-10s %-10s %-10s %-10s %-10s\n" 29 30
+
+geshi="%-10s %-10s %-10s %-10s %-10s %-10s %-10s\n"
+
+printf "\e[1;32m${geshi}\e[0m\n\e[1;33m${geshi}\e[0m\n\e[1;34m${geshi}\e[0m\n\e[1;35m${geshi}\e[0m\n\e[1;36m${geshi}\e[0m" 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30
